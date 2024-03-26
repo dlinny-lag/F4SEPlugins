f9606fb22843464b52296b97ca17874537f8a67c
dlinny-lag <dlinny.lag@gmail.com>
2024-03-26 15:23:39 +0500

    AA: version 1.0.0. Added ESL flag to AA.ESM file

--- AA/PluginAPIExport.hpp
+++ AA/PluginAPIExport.hpp
@@ -9,8 +9,8 @@ namespace PluginAPIExport
 
 	const char* pluginName = "AA";
 	const UInt32 pluginUID = 'AA';
-	const UInt32 pluginVersionInt = 0x0090;
-	const char* pluginVersionString = "0.9.0";
+	const UInt32 pluginVersionInt = 0x0100;
+	const char* pluginVersionString = "1.0.0";
 
 	BSFixedString GetVersionString(StaticFunctionTag* _)
 	{
