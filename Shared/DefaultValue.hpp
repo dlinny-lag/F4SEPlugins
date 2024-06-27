cd2f0364e21e8e589391040fcffacd80aa1ad44f
dlinny-lag <dlinny.lag@gmail.com>
2024-06-27 18:50:20 +0500

    added default value implementation

---
+++ Shared/DefaultValue.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+template <class T>
+struct DefaultValue
+{
+	static T Get() {return {};}
+};
+
+#include "TypeDefs.h"
+
+template<>
+struct DefaultValue<FORM_TYPE>
+{
+	static FORM_TYPE Get() { return nullptr;}
+};
+
+#include "f4se/PapyrusArgs.h"
+
+template<>
+struct DefaultValue<VMVariable>
+{
+	static VMVariable Get()
+	{
+		VMVariable retVal;
+		retVal.GetValue().SetNone();
+		return retVal;
+	}
+};
